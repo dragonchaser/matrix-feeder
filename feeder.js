8d9bba15010777c1347f4b760cc3d05d6f1c7e0f
Christian <crichter@suse.com>
2020-10-20 16:13:48 +0200

    add youtube links

--- feeder.js
+++ feeder.js
@@ -1,3 +1,5 @@
+const cheerio = require('cheerio')
+const request = require('request')
 const jsonfile = require('jsonfile')
 const sdk = require("matrix-bot-sdk");
 
@@ -22,13 +24,41 @@ client.on("room.message", (roomId, event) => {
   const info = event["content"]["info"]
   const url = event["content"]["url"]
 
-  if(roomId != config.targetRoomId && config.relayTypes.includes(type)) {
-    client.sendMessage(config.targetRoomId, {
-      "sender" : sender,
-      "msgtype": type,
-      "body": body,
-      "info": info,
-      "url": url
-  });
+  if(roomId != config.targetRoomId) {
+    if(config.relayTypes.includes(type)) {
+      client.sendMessage(config.targetRoomId, {
+        "sender" : sender,
+        "msgtype": type,
+        "body": body,
+        "info": info,
+        "url": url
+      });
+    }
+    if(config.extractYoutubeLinks && type == "m.text") {
+      link = body.match(/https?:\/\/[^\ ]*youtu[^\ ]*/g)[0]
+      msg = link
+      if(config.showYoutubeTitle) {
+        request(link, function(err, _res, body) {
+          if (err) return console.error(err);
+          title = ""
+          let $ = cheerio.load(body);
+          title = $('title').text();
+          if(title != "") {
+            msg = `${link} - ${title}`
+          } else {
+            msg = `${link}`
+          }
+          client.sendMessage(config.targetRoomId, {
+            "msgtype": "m.text",
+            "body": msg,
+          });
+        })
+      } else {
+        client.sendMessage(config.targetRoomId, {
+          "msgtype": "m.text",
+          "body": msg,
+        });
+      }
+    }
   }
 });
