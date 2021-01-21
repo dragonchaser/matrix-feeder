8efb4f8332499498fc65eb4f7e49475ae03c4ace
devdevjeff <77796011+devdevjeff@users.noreply.github.com>
2021-01-21 17:33:56 +0100

    Update feeder.js

--- feeder.js
+++ feeder.js
@@ -23,42 +23,16 @@ client.on("room.message", (roomId, event) => {
   const type = event["content"]["msgtype"];
   const info = event["content"]["info"]
   const url = event["content"]["url"]
-
-  if(roomId != config.targetRoomId) {
+  
+  for (i in config.targetRoomId) {
+  if(roomId != config.targetRoomId[i]) {
     if(config.relayTypes.includes(type)) {
-      client.sendMessage(config.targetRoomId, {
-        "sender" : sender,
+      client.sendMessage(config.targetRoomId[i], {
         "msgtype": type,
         "body": body,
         "info": info,
         "url": url
       });
-    }
-    if(config.extractYoutubeLinks && type == "m.text") {
-      link_matches = body.match(/https?:\/\/[^\ ]*youtu[^\ ]*/g)
-      if( link_matches && link_matches.length > 0) {
-        msg = link_matches[0]
-        if(config.showYoutubeTitle) {
-          request(msg, function(err, _res, body) {
-            if (err) return console.error(err);
-            title = ""
-            let $ = cheerio.load(body);
-            title = $('title').text();
-            if(title != "") {
-              msg = `${msg} - ${title}`
-            }
-            client.sendMessage(config.targetRoomId, {
-              "msgtype": "m.text",
-              "body": msg,
-            });
-          })
-        } else {
-          client.sendMessage(config.targetRoomId, {
-            "msgtype": "m.text",
-            "body": msg,
-          });
-        }
-      }
     }
   }
 });
